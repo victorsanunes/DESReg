6c5babc5da53d8311ca3f1d586d0c15c0554f74b
Victor Nunes <victor.desanunes@gmail.com>
2025-04-22 10:17:56 -0300

    added pandas import

--- Examples/change_parameters.py
+++ Examples/change_parameters.py
@@ -2,6 +2,7 @@ from desReg.des.DESRegression import DESRegression
 from desReg.dataset import load_Student_Mark
 
 import numpy as np
+import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
